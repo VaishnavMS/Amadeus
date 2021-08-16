be307a276591c7f7fd337ecb9bdd3ca0ec934544
Vaishnav <66957152+VaishnavMS@users.noreply.github.com>
2021-08-16 11:44:06 +0530

    Update multiuse.js

--- plugins/multiuse.js
+++ plugins/multiuse.js
@@ -10,8 +10,8 @@ Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: IG_DESC }, async
     //if(match[1] == '') return
     let { data, type } = await instaGram(match[1], 'f3eaf19231f6201c');
     //if(type == undefined) return 
-    if (type === 'image') { await message.sendMessage(data, MessageType.image, { caption: "```by kurisu```" }) }
-    else if (type === 'video') { await message.sendMessage(data, MessageType.video, { caption: "```BY kurisu```" }) }
+    if (type === 'image') { await message.sendMessage(data, MessageType.image, { caption: "```Downloaded by KURISU```" }) }
+    else if (type === 'video') { await message.sendMessage(data, MessageType.video, { caption: "```downloaded by KURISU```" }) }
 });
 //const axios = require('axios')
 async function instaGram(url, key){
